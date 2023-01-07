3d92b5fb331ef623ca486ff34d8b29e6613f7fa3
Ronie Casaclang <101075980+ronie-casaclang@users.noreply.github.com>
2023-01-08 00:30:36 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,142 @@
+
+//Song list Array
+var list = [];
+list[1] = "3XwgjFunA3c";
+list[2] = "4MRztxyGKpE";
+list[3] = "srBTyNMp1SE";
+list[4] = "YgARXxFxT7Y";
+list[5] = "96qxxsJrQkQ";
+list[6] = "acPB4mDoRZY";
+list[7] = "e043mP9ul1Q";
+list[8] = "smLHXy5Bph8";
+list[9] = "814m-dhwS6s";
+list[10] = "yBhgO0plkhE";
+list[11] = "6j7JDJvKMiM";
+list[12] = "hNmgGtSTqb8";
+list[13] = "rXohLV8fYIQ";
+list[14] = "EVREose0mwc";
+list[15] = "4CpPuTgY4jU";
+list[16] = "lt5h2bVlgCY";
+list[17] = "y-k2tvfwiYw";
+list[18] = "bJbAK8zacVI";
+list[19] = "ZLJPFx-NPhg";
+list[20] = "H1Ae8yNe4r0";
+
+//Title
+document.getElementById("lbl_title1").innerText = "Wala kang Katulad";
+document.getElementById("lbl_title2").innerText = "I Need You More Today";
+document.getElementById("lbl_title3").innerText = "Thinking Out Loud";
+document.getElementById("lbl_title4").innerText = "The Day You Said Goodnight";
+document.getElementById("lbl_title5").innerText = "Kay Tagal Kitang Hinintay";
+document.getElementById("lbl_title6").innerText = "Perfect";
+document.getElementById("lbl_title7").innerText = "Jopay";
+document.getElementById("lbl_title8").innerText = "Yakap sa Dilim";
+document.getElementById("lbl_title9").innerText = "Sana";
+document.getElementById("lbl_title10").innerText = "Baby";
+document.getElementById("lbl_title11").innerText = "Hanggang Kailan";
+document.getElementById("lbl_title12").innerText = "214";
+document.getElementById("lbl_title13").innerText = "Hinahanap-hanap kita";
+document.getElementById("lbl_title14").innerText = "All of me";
+document.getElementById("lbl_title15").innerText = "Always be my Baby";
+document.getElementById("lbl_title16").innerText = "Close You and I";
+document.getElementById("lbl_title17").innerText = "Babalik Sayo";
+document.getElementById("lbl_title18").innerText = "Having You Near me";
+document.getElementById("lbl_title19").innerText = "Siguro Nga";
+document.getElementById("lbl_title20").innerText = "Dance with my Father";
+
+//Artist
+document.getElementById("lbl_singer1").innerText = "Spongecola";
+document.getElementById("lbl_singer2").innerText = "Caleb Santos";
+document.getElementById("lbl_singer3").innerText = "Ed Sheeran";
+document.getElementById("lbl_singer4").innerText = "Hale";
+document.getElementById("lbl_singer5").innerText = "Spongecola";
+document.getElementById("lbl_singer6").innerText = "Ed Sheeran";
+document.getElementById("lbl_singer7").innerText = "Mayonnaise";
+document.getElementById("lbl_singer8").innerText = "Orange & Lemons";
+document.getElementById("lbl_singer9").innerText = "I Belong to the Zoo";
+document.getElementById("lbl_singer10").innerText = "Justin Bieber";
+document.getElementById("lbl_singer11").innerText = "Orange & Lemons";
+document.getElementById("lbl_singer12").innerText = "Rivermaya";
+document.getElementById("lbl_singer13").innerText = "Rivermaya";
+document.getElementById("lbl_singer14").innerText = "John Legend";
+document.getElementById("lbl_singer15").innerText = "David Cook";
+document.getElementById("lbl_singer16").innerText = "Spongecola";
+document.getElementById("lbl_singer17").innerText = "Moira Dela Torre";
+document.getElementById("lbl_singer18").innerText = "Air Supply";
+document.getElementById("lbl_singer19").innerText = "Spongecola";
+document.getElementById("lbl_singer20").innerText = "Luther Vandross";
+
+//Images
+document.getElementById("img_1").src = "https://cdns-images.dzcdn.net/images/cover/0c3d1704afd37656fd074c448af28f82/264x264.jpg";
+document.getElementById("img_2").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR-rVEHH5oI7hr8Kyk9PUdgg6zoWrAavjXlDtdW8lDg9WuLwGg2BSAYe6Irxdy1x0cBfGM&usqp=CAU";
+document.getElementById("img_3").src = "https://c-fa.cdn.smule.com/rs-s25/arr/dc/fd/fd9ef0bc-0795-4973-b5d1-69b42e670904_1024.jpg";
+document.getElementById("img_4").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMRVMXUt1YpuYNstQ4591h7ay9I3RT_JCz8A&usqp=CAU";
+document.getElementById("img_5").src = "https://cdns-images.dzcdn.net/images/cover/0c3d1704afd37656fd074c448af28f82/264x264.jpg";
+document.getElementById("img_6").src = "https://upload.wikimedia.org/wikipedia/en/8/80/Ed_Sheeran_Perfect_Single_cover.jpg";
+document.getElementById("img_7").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbjdukIcEaaRhuVeiXM7BRjvFP26e_ryxNJfUGddZsDQcqbGVo88KUL5itd26vJ2zQ3EY&usqp=CAU";
+document.getElementById("img_8").src = "https://i1.sndcdn.com/artworks-000145929118-esrfuc-t500x500.jpg";
+document.getElementById("img_9").src = "https://i.discogs.com/V2g47QpOKMrO-1vSd8vil8xPU_2miUz2v7qFjOgM0dU/rs:fit/g:sm/q:40/h:300/w:300/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE0ODY4/MDAxLTE1ODMxMTAw/MDAtMTU2MS5qcGVn.jpeg";
+document.getElementById("img_10").src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPISmC_ugnk0SoDlHb1ldQbiPFERk0apkYGdDs0LFgQSOhUWST6NoiWA1EAqkhr6IrZEg&usqp=CAU";
+document.getElementById("img_11").src = "https://i1.sndcdn.com/artworks-000145929118-esrfuc-t500x500.jpg";
+document.getElementById("img_12").src = "https://pianosheetmusiconline.com/wp-content/uploads/2013/04/214-Rivermaya.jpg";
+document.getElementById("img_13").src = "https://cdns-images.dzcdn.net/images/cover/5379d9319d8fb36a9f481ff906227bc1/264x264.jpg";
+document.getElementById("img_14").src = "https://i1.sndcdn.com/artworks-000324497780-jib6dz-t500x500.jpg";
+document.getElementById("img_15").src = "https://www.whosampled.com/static/track_images_200/lr87731_2013722_173513653862.jpg";
+document.getElementById("img_16").src = "https://cdns-images.dzcdn.net/images/cover/0c3d1704afd37656fd074c448af28f82/264x264.jpg";
+document.getElementById("img_17").src = "https://data-corporate.abs-cbn.com/corp/medialibrary/dotcom/ncstpr%20star%20music%202022/0722%20-%20moira%20babalik%20sayo/babalik%20sayo%20_%20moira%20dela%20torre.jpg";
+document.getElementById("img_18").src = "https://cdn-s3.allmusic.com/release-covers/400/0000/114/0000114539.jpg";
+document.getElementById("img_19").src = "https://cdns-images.dzcdn.net/images/cover/0c3d1704afd37656fd074c448af28f82/264x264.jpg";
+document.getElementById("img_20").src = "https://i.scdn.co/image/ab67616d00001e022ff5c4b27f21a58953871c80";
+
+//Buttons
+document.getElementById("btn_play1").onclick = function(){ btnSelect_Song(1); };
+document.getElementById("btn_play2").onclick = function(){ btnSelect_Song(2); };
+document.getElementById("btn_play3").onclick = function(){ btnSelect_Song(3); };
+document.getElementById("btn_play4").onclick = function(){ btnSelect_Song(4); };
+document.getElementById("btn_play5").onclick = function(){ btnSelect_Song(5); };
+document.getElementById("btn_play6").onclick = function(){ btnSelect_Song(6); };
+document.getElementById("btn_play7").onclick = function(){ btnSelect_Song(7); };
+document.getElementById("btn_play8").onclick = function(){ btnSelect_Song(8); };
+document.getElementById("btn_play9").onclick = function(){ btnSelect_Song(9); };
+document.getElementById("btn_play10").onclick = function(){ btnSelect_Song(10); };
+document.getElementById("btn_play11").onclick = function(){ btnSelect_Song(11); };
+document.getElementById("btn_play12").onclick = function(){ btnSelect_Song(12); };
+document.getElementById("btn_play13").onclick = function(){ btnSelect_Song(13); };
+document.getElementById("btn_play14").onclick = function(){ btnSelect_Song(14); };
+document.getElementById("btn_play15").onclick = function(){ btnSelect_Song(15); };
+document.getElementById("btn_play16").onclick = function(){ btnSelect_Song(16); };
+document.getElementById("btn_play17").onclick = function(){ btnSelect_Song(17); };
+document.getElementById("btn_play18").onclick = function(){ btnSelect_Song(18); };
+document.getElementById("btn_play19").onclick = function(){ btnSelect_Song(19); };
+document.getElementById("btn_play20").onclick = function(){ btnSelect_Song(20); };
+
+//Selection Method
+var Video = document.getElementById("vid_video");
+function btnSelect_Song(num){
+    var song = "";
+    switch(num){
+        case 1: song = list[1]; break;
+        case 2: song = list[2]; break;
+        case 3: song = list[3]; break;
+        case 4: song = list[4]; break;
+        case 5: song = list[5]; break;
+        case 6: song = list[6]; break;
+        case 7: song = list[7]; break;
+        case 8: song = list[8]; break;
+        case 9: song = list[9]; break;
+        case 10: song = list[10]; break;
+        case 11: song = list[11]; break;
+        case 12: song = list[12]; break;
+        case 13: song = list[13]; break;
+        case 14: song = list[14]; break;
+        case 15: song = list[15]; break;
+        case 16: song = list[16]; break;
+        case 17: song = list[17]; break;
+        case 18: song = list[18]; break;
+        case 19: song = list[19]; break;
+        case 20: song = list[20]; break;
+    }
+    displayControls();
+    Video.src = "https://www.youtube.com/embed/" + song;
+    Video.play();
+}
